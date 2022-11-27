6a0d7d9596caaae8b8217ed5246eff736120ef22
Anden Acitelli <a.acitelli@gmail.com>
2022-11-27 15:01:03 -0500

    Initial backend implementation should be just about complete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,34 +1,44 @@
+// TODO: Split business logic into its own layer that'll be between this and persistence layer
+// TODO: Write unit tests https://firebase.google.com/docs/functions/unit-testing
+
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { Listing, ListingPayload } from "./types";
+import { ListingPayload } from "./types";
 import * as express from "express";
 import { validateListing } from "./schema";
+import { addListing, getListings, isDuplicateListing } from "./persistence";
 
 const app = express();
-
 admin.initializeApp(functions.config().firebase);
 
-const LISTINGS_COLLECTION = "listings";
 
 // 1. Create Listing - <region, server, item, character, commission> tuple
 app.post("/listings",
     async (request, response) => {
-        const db = admin.firestore();
         switch (request.method) {
             case "POST": {
+
+                // Validate payload
                 const payload = request.body as ListingPayload;
                 const valid = validateListing(payload);
                 if (!valid) {
                     response.status(400).send(validateListing.errors);
                     return;
                 }
-                await db.collection(LISTINGS_COLLECTION).add(payload);
+
+                // Check to see if duplicate
+                // TODO: If I check for this, users need a way to delete and re-list. This requires some form of authentication - it's probably better to just go straight for Battle.net instead of trying Firebase.
+                if (await isDuplicateListing(payload)) {
+                    response.sendStatus(409);
+                    return;
+                }
+
+                await addListing(payload);
                 functions.logger.debug(`Successfully Posted: ${JSON.stringify(payload)}`);
                 response.sendStatus(200);
                 break;
             }
             default: {
-                functions.logger.warn(`Illegal method ${request.method} on endpoint /listing`);
                 response.sendStatus(405);
                 break;
             }
@@ -37,13 +47,9 @@ app.post("/listings",
 
 // 2. Get Items for Realm
 app.get("/:region/:realm/items", async (request, response) => {
-    const db = admin.firestore();
     switch (request.method) {
         case "GET": {
-            const listings = (await db.collection(LISTINGS_COLLECTION).get()).docs.map(doc => {
-                return doc.data() as Listing;
-            })
-            functions.logger.debug(`Retrieved Listings: ${JSON.stringify(listings)}`);
+            const listings = await getListings();
             response.send(listings.filter(listing => {
                 return listing.seller.region === request.params.region &&
                     listing.seller.realm === request.params.realm
@@ -51,7 +57,6 @@ app.get("/:region/:realm/items", async (request, response) => {
             break;
         }
         default: {
-            functions.logger.warn(`Illegal method ${request.method} on endpoint /:region/:realm/items`);
             response.sendStatus(405);
             break;
         }
@@ -60,14 +65,9 @@ app.get("/:region/:realm/items", async (request, response) => {
 
 // 3. Get Listings for Item
 app.get("/:region/:realm/item/:itemId", async (request, response) => {
-    const db = admin.firestore();
     switch (request.method) {
         case "GET": {
-            const listings = (await db.collection(LISTINGS_COLLECTION).get()).docs.map(doc => {
-                return doc.data() as Listing;
-            })
-            functions.logger.debug(`Retrieved Listings: ${JSON.stringify(listings)}`);
-
+            const listings = await getListings();
             response.send(listings.filter(listing => {
                 return listing.seller.region === request.params.region &&
                     listing.seller.realm === request.params.realm &&
@@ -76,7 +76,6 @@ app.get("/:region/:realm/item/:itemId", async (request, response) => {
             break;
         }
         default: {
-            functions.logger.warn(`Illegal method ${request.method} on endpoint /:region/:realm/:itemId`);
             response.sendStatus(405);
             break;
         }
---
+++ functions/src/persistence.ts
@@ -0,0 +1,26 @@
+import * as admin from "firebase-admin";
+import { Listing, ListingPayload } from "./types";
+
+const LISTINGS_COLLECTION = "listings";
+export const getListings = async () => {
+    const db = admin.firestore();
+    return (await db.collection(LISTINGS_COLLECTION).get()).docs.map(doc => {
+        return doc.data() as Listing;
+    })
+}
+
+export const addListing = async (listing: ListingPayload) => {
+    const db = admin.firestore();
+    await db.collection(LISTINGS_COLLECTION).add(listing);
+}
+
+export const isDuplicateListing = async (listing: ListingPayload) => {
+    const db = admin.firestore();
+    const listings = await db.collection(LISTINGS_COLLECTION)
+        .where("seller.region", "==", listing.seller.region)
+        .where("seller.realm", "==", listing.seller.realm)
+        .where("seller.characterName", "==", listing.seller.characterName)
+        .where("itemId", "==", listing.itemId)
+        .get();
+    return listings.docs.length > 0;
+}

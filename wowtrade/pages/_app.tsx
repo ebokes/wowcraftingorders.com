1d2f7a1d40f31a17070543292bcaad7b2016922d
Anden Acitelli <a.acitelli@gmail.com>
2022-11-27 15:39:59 -0500

    Misc additions

--- wowtrade/pages/_app.tsx
+++ wowtrade/pages/_app.tsx
@@ -1,6 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return <div>
+
+        {/* TODO: Update measurement ID once I choose a domain */}
+        <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=ABCD`}
+        />
+        <Script strategy="lazyOnload" id="ga-script">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'ABCD', {
+              page_path: window.location.pathname,
+            });
+        `}
+        </Script>
+        <Component {...pageProps} />
+    </div>
 }

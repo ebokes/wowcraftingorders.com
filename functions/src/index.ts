e4aa542c7795599eaf2b86c7df8c7db48358a364
Anden Acitelli <a.acitelli@gmail.com>
2022-11-27 16:37:22 -0500

    Should fix CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,6 +7,9 @@ import { ListingPayload } from "./types";
 import * as express from "express";
 import { validateListing } from "./schema";
 import { addListing, getListings, isDuplicateListing } from "./persistence";
+import * as cors from "cors";
+
+const corsObject = cors();
 
 const app = express();
 admin.initializeApp(functions.config().firebase);
@@ -15,72 +18,79 @@ admin.initializeApp(functions.config().firebase);
 // 1. Create Listing - <region, server, item, character, commission> tuple
 app.post("/listings",
     async (request, response) => {
-        switch (request.method) {
-            case "POST": {
-                // Validate payload
-                const payload = request.body as ListingPayload;
-                const valid = validateListing(payload);
-                if (!valid) {
-                    response.status(400).send(validateListing.errors);
-                    return;
+        corsObject(request, response, async (err) => {
+            switch (request.method) {
+                case "POST": {
+                    // Validate payload
+                    const payload = request.body as ListingPayload;
+                    const valid = validateListing(payload);
+                    if (!valid) {
+                        response.status(400).send(validateListing.errors);
+                        return;
+                    }
+
+                    // Check to see if duplicate
+                    // TODO: If I check for this, users need a way to delete and re-list. This requires some form of authentication - it's probably better to just go straight for Battle.net instead of trying Firebase.
+                    if (await isDuplicateListing(payload)) {
+                        response.sendStatus(409);
+                        return;
+                    }
+
+                    await addListing(payload);
+                    functions.logger.debug(`Successfully Posted: ${JSON.stringify(payload)}`);
+
+                    // TODO: Modify CORS to point towards my actual domain
+                    response.setHeader("Access-Control-Allow-Origin", "*").sendStatus(200);
+                    break;
                 }
-
-                // Check to see if duplicate
-                // TODO: If I check for this, users need a way to delete and re-list. This requires some form of authentication - it's probably better to just go straight for Battle.net instead of trying Firebase.
-                if (await isDuplicateListing(payload)) {
-                    response.sendStatus(409);
-                    return;
+                default: {
+                    response.sendStatus(405);
+                    break;
                 }
-
-                await addListing(payload);
-                functions.logger.debug(`Successfully Posted: ${JSON.stringify(payload)}`);
-
-                // TODO: Modify CORS to point towards my actual domain
-                response.setHeader("Access-Control-Allow-Origin", "*").sendStatus(200);
-                break;
             }
-            default: {
-                response.sendStatus(405);
-                break;
-            }
-        }
+        })
+
     });
 
 // 2. Get Items for Realm
 app.get("/:region/:realm/items", async (request, response) => {
-    switch (request.method) {
-        case "GET": {
-            const listings = await getListings();
-            response.send(listings.filter((listing) => {
-                return listing.seller.region === request.params.region &&
-                    listing.seller.realm === request.params.realm;
-            }));
-            break;
+    corsObject(request, response, async (err) => {
+        switch (request.method) {
+            case "GET": {
+                const listings = await getListings();
+                response.send(listings.filter((listing) => {
+                    return listing.seller.region === request.params.region &&
+                        listing.seller.realm === request.params.realm;
+                }));
+                break;
+            }
+            default: {
+                response.setHeader("Access-Control-Allow-Origin", "*").sendStatus(405);
+                break;
+            }
         }
-        default: {
-            response.setHeader("Access-Control-Allow-Origin", "*").sendStatus(405);
-            break;
-        }
-    }
+    });
 });
 
 // 3. Get Listings for Item
 app.get("/:region/:realm/item/:itemId", async (request, response) => {
-    switch (request.method) {
-        case "GET": {
-            const listings = await getListings();
-            response.send(listings.filter((listing) => {
-                return listing.seller.region === request.params.region &&
-                    listing.seller.realm === request.params.realm &&
-                    listing.itemId === parseInt(request.params.itemId);
-            }));
-            break;
+    corsObject(request, response, async (err) => {
+        switch (request.method) {
+            case "GET": {
+                const listings = await getListings();
+                response.send(listings.filter((listing) => {
+                    return listing.seller.region === request.params.region &&
+                        listing.seller.realm === request.params.realm &&
+                        listing.itemId === parseInt(request.params.itemId);
+                }));
+                break;
+            }
+            default: {
+                response.setHeader("Access-Control-Allow-Origin", "*").sendStatus(405);
+                break;
+            }
         }
-        default: {
-            response.setHeader("Access-Control-Allow-Origin", "*").sendStatus(405);
-            break;
-        }
-    }
+    });
 });
 
 exports.app = functions.https.onRequest(app);

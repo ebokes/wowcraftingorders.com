e6fd99f024f3a2c3c1df88f6b1488b276a13765d
Anden Acitelli <a.acitelli@gmail.com>
2022-11-28 13:38:58 -0500

    Remove unnecessary API route

--- wowtrade/pages/index.tsx
+++ wowtrade/pages/index.tsx
@@ -39,7 +39,9 @@ export default function Home() {
             <main className={"p-3"}>
                 <h3>What Is This?</h3>
                 <p>WowTrade.xyz is a marketplace for high-quality work orders in World of Warcraft (WoW). Public work
-                    orders don't guarantee a quality, so this site enables people to sell high-quality work orders for a
+                    orders don't guarantee a quality (if you're unfamiliar with the new crafting system, read <a
+                        href={"https://www.wowhead.com/guide/professions/overview-dragonflight"}>this page on
+                        Wowhead</a>), so this site is intended to enable people to sell high-quality work orders for a
                     higher amount, without needing to spam trade chat.</p>
 
                 <Form style={{ width: "100%" }}>

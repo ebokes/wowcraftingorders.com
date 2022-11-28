99531ccfa99546b40a576d5584eebd04eaacdac2
Anden Acitelli <a.acitelli@gmail.com>
2022-11-27 20:33:27 -0500

    Basic form inputs

--- wowtrade/pages/_app.tsx
+++ wowtrade/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from "next/script";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { SWRConfig } from "swr";
 
 let ROOT_URL: string;
@@ -35,15 +35,18 @@ export default function App({ Component, pageProps }: AppProps) {
             id={'custom'}>{`var whTooltips = whTooltips || { colorLinks: true, iconizeLinks: true, renameLinks: true};`}</script>
         <Script strategy={"afterInteractive"} src={'https://wow.zamimg.com/js/tooltips.js'}/>
 
-        <Col md={2}></Col>
-        <Col md={8}>
-            <SWRConfig value={{
-                fetcher: (url) => fetch(ROOT_URL + url).then(r => r.json())
-            }}>
-                <Component {...pageProps} />
-            </SWRConfig>
-        </Col>
-        <Col md={2}></Col>
+        <Row>
+            <Col md={1}></Col>
+            <Col md={10}>
+                <SWRConfig value={{
+                    fetcher: (url) => fetch(ROOT_URL + url).then(r => r.json())
+                }}>
+                    <Component {...pageProps} />
+                </SWRConfig>
+            </Col>
+            <Col md={1}></Col>
+        </Row>
+
 
     </div>
 }
--- wowtrade/pages/index.tsx
+++ wowtrade/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useState } from "react";
 import ListingsList from "../components/ListingsList";
+import { Col, Form, InputGroup, Row } from "react-bootstrap";
 
 export default function Home() {
     const [region, setRegion] = useState("en");
@@ -15,7 +16,27 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <main className={styles.main}>
+            <main className={"p-3"}>
+                <Form style={{ width: "100%" }}>
+                    <Row>
+                        <Col>
+                            <Form.Label>Region</Form.Label>
+                            <Form.Control as="select" value={region} onChange={(e) => setRegion(e.target.value)}>
+                                <option value="en">EN (Americas)</option>
+                                <option value="eu">EU (Europe)</option>
+                                <option value="kr">KR (Korea)</option>
+                                <option value="tw">TW (Taiwan)</option>
+                            </Form.Control>
+                        </Col>
+                        <Col>
+                            <Form.Label>Realm</Form.Label>
+                            <Form.Control value={realm} onChange={(e) => setRealm(e.target.value)}
+                                          placeholder={"Area 52"}></Form.Control>
+                        </Col>
+                    </Row>
+                    <InputGroup>
+                    </InputGroup>
+                </Form>
                 <ListingsList region={region} realm={realm}/>
             </main>
         </div>

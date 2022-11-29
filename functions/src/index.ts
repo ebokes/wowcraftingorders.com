dc73725eea86a9296b1d98ffe4eaed1396687ca1
Anden Acitelli <a.acitelli@gmail.com>
2022-11-28 19:07:54 -0500

    Fixed things, I think?

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,23 @@
 // TODO: Split business logic into its own layer that'll be between this and persistence layer
 // TODO: Write unit tests https://firebase.google.com/docs/functions/unit-testing
 
-import * as dotenv from 'dotenv';
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import { initializeApp } from "firebase-admin/app";
 import { ListingPayload } from "./types";
 import * as express from "express";
 import { validateListing } from "./ListingSchema";
 import { addListing, getListings, isDuplicateListing } from "./persistence";
-import * as passport from "passport";
+
+import { config } from "dotenv";
 
 import { defineString } from "firebase-functions/params";
+
+const passport = require("passport");
 
 const BATTLENET_CLIENT_ID = defineString("BATTLENET_CLIENT_ID");
 const BATTLENET_CLIENT_SECRET = defineString("BATTLENET_CLIENT_SECRET");
 
-dotenv.config();
+config();
 
 const cors = require('cors')({ origin: true });
 const app = express();
@@ -24,7 +26,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // @ts-ignore
-passport.use(new require('passport-bnet').Strategy({
+const BNetStrategy = require("passport-bnet").Strategy;
+passport.use(new BNetStrategy({
     clientID: BATTLENET_CLIENT_ID,
     clientSecret: BATTLENET_CLIENT_SECRET,
     callbackUrl: "https://wowtrade.xyz",
@@ -34,49 +37,49 @@ passport.use(new require('passport-bnet').Strategy({
     return done(null, profile);
 }));
 
-admin.initializeApp(functions.config().firebase);
+initializeApp(functions.config().firebase);
 
 // 1. Create Listing - <region, server, item, character, commission> tuple
 // TODO: Validate the Battle.net access token; verify they own the character
 app.post("/listings",
     async (request, response) => {
-        passport.authenticate('bnet', { session: false }, async (err, user) => {
-            functions.logger.info("User: " + JSON.stringify(user));
-            switch (request.method) {
-                case "POST": {
-                    // Validate payload
-                    const payload = request.body as ListingPayload;
-                    let valid = validateListing(payload);
+        // passport.authenticate('bnet', { session: false }, async (err: any, user: any) => {
+        // functions.logger.info("User: " + JSON.stringify(user));
+        switch (request.method) {
+            case "POST": {
+                // Validate payload
+                const payload = request.body as ListingPayload;
+                let valid = validateListing(payload);
 
-                    // Manually handling because the logic is a bit complicated
-                    // TODO: See if I can build this directly into AJV
-                    if (!payload.commission.gold && !payload.commission.silver && !payload.commission.copper) {
-                        valid = false;
-                        return response.status(400).send([{ message: "Commission must be nonzero." }]);
-                    } else if (!valid) {
-                        if (!validateListing.errors) {
-                            functions.logger.error(`Unknown issue validating Listing payload: ${JSON.stringify(payload)}`)
-                            return response.status(400).send([{ message: "Unknown error. Please verify all fields are filled out and correct." }]);
-                        } else {
-                            return response.status(400).send(validateListing.errors);
-                        }
+                // Manually handling because the logic is a bit complicated
+                // TODO: See if I can build this directly into AJV
+                if (!payload.commission.gold && !payload.commission.silver && !payload.commission.copper) {
+                    valid = false;
+                    return response.status(400).send([{ message: "Commission must be nonzero." }]);
+                } else if (!valid) {
+                    if (!validateListing.errors) {
+                        functions.logger.error(`Unknown issue validating Listing payload: ${JSON.stringify(payload)}`)
+                        return response.status(400).send([{ message: "Unknown error. Please verify all fields are filled out and correct." }]);
+                    } else {
+                        return response.status(400).send(validateListing.errors);
                     }
-
-                    // Check to see if duplicate
-                    // TODO: If I check for this, users need a way to delete and re-list. This requires some form of authentication - it's probably better to just go straight for Battle.net instead of trying Firebase.
-                    if (await isDuplicateListing(payload)) {
-                        return response.sendStatus(409);
-                    }
-
-                    await addListing(payload);
-                    functions.logger.debug(`Successfully created Listing: ${JSON.stringify(payload)}`);
-                    return response.sendStatus(201);
                 }
-                default: {
-                    return response.sendStatus(405);
+
+                // Check to see if duplicate
+                // TODO: If I check for this, users need a way to delete and re-list. This requires some form of authentication - it's probably better to just go straight for Battle.net instead of trying Firebase.
+                if (await isDuplicateListing(payload)) {
+                    return response.sendStatus(409);
                 }
+
+                await addListing(payload);
+                functions.logger.debug(`Successfully created Listing: ${JSON.stringify(payload)}`);
+                return response.sendStatus(201);
             }
-        });
+            default: {
+                return response.sendStatus(405);
+            }
+        }
+        // });
     });
 
 // 2. Get Items for Realm

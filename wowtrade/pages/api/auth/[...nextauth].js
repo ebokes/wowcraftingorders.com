01590999ef9bb761bcccc6835bca79eaa9ab6e64
Anden Acitelli <a.acitelli@gmail.com>
2022-11-29 00:13:02 -0500

    Try something new

--- wowtrade/pages/api/auth/[...nextauth].js
+++ wowtrade/pages/api/auth/[...nextauth].js
@@ -9,6 +9,7 @@ export const authOptions = {
             clientId: process.env.BATTLENET_CLIENT_ID,
             clientSecret: process.env.BATTLENET_CLIENT_SECRET,
             issuer: process.env.BATTLENET_ISSUER,
+            scope: "wow.profile",
             // authorization: {
             //     params: {scope: "wow.profile"},
             // },

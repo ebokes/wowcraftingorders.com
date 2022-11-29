f58af40a37debf0e35093ed8853c6a4520134a57
Anden Acitelli <a.acitelli@gmail.com>
2022-11-28 19:35:56 -0500

    Potential fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -45,43 +45,43 @@ initializeApp(functions.config().firebase);
 // TODO: Validate the Battle.net access token; verify they own the character
 app.post("/listings",
     async (request, response) => {
-        // passport.authenticate('bnet', { session: false }, async (err: any, user: any) => {
-        // functions.logger.info("User: " + JSON.stringify(user));
-        switch (request.method) {
-            case "POST": {
-                // Validate payload
-                const payload = request.body as ListingPayload;
-                let valid = validateListing(payload);
+        passport.authenticate('bnet', { session: false }, async (err: any, user: any) => {
+            functions.logger.info("User: " + JSON.stringify(user));
+            switch (request.method) {
+                case "POST": {
+                    // Validate payload
+                    const payload = request.body as ListingPayload;
+                    let valid = validateListing(payload);
 
-                // Manually handling because the logic is a bit complicated
-                // TODO: See if I can build this directly into AJV
-                if (!payload.commission.gold && !payload.commission.silver && !payload.commission.copper) {
-                    valid = false;
-                    return response.status(400).send([{ message: "Commission must be nonzero." }]);
-                } else if (!valid) {
-                    if (!validateListing.errors) {
-                        functions.logger.error(`Unknown issue validating Listing payload: ${JSON.stringify(payload)}`)
-                        return response.status(400).send([{ message: "Unknown error. Please verify all fields are filled out and correct." }]);
-                    } else {
-                        return response.status(400).send(validateListing.errors);
+                    // Manually handling because the logic is a bit complicated
+                    // TODO: See if I can build this directly into AJV
+                    if (!payload.commission.gold && !payload.commission.silver && !payload.commission.copper) {
+                        valid = false;
+                        return response.status(400).send([{ message: "Commission must be nonzero." }]);
+                    } else if (!valid) {
+                        if (!validateListing.errors) {
+                            functions.logger.error(`Unknown issue validating Listing payload: ${JSON.stringify(payload)}`)
+                            return response.status(400).send([{ message: "Unknown error. Please verify all fields are filled out and correct." }]);
+                        } else {
+                            return response.status(400).send(validateListing.errors);
+                        }
                     }
-                }
 
-                // Check to see if duplicate
-                // TODO: If I check for this, users need a way to delete and re-list. This requires some form of authentication - it's probably better to just go straight for Battle.net instead of trying Firebase.
-                if (await isDuplicateListing(payload)) {
-                    return response.sendStatus(409);
-                }
+                    // Check to see if duplicate
+                    // TODO: If I check for this, users need a way to delete and re-list. This requires some form of authentication - it's probably better to just go straight for Battle.net instead of trying Firebase.
+                    if (await isDuplicateListing(payload)) {
+                        return response.sendStatus(409);
+                    }
 
-                await addListing(payload);
-                functions.logger.debug(`Successfully created Listing: ${JSON.stringify(payload)}`);
-                return response.sendStatus(201);
+                    await addListing(payload);
+                    functions.logger.debug(`Successfully created Listing: ${JSON.stringify(payload)}`);
+                    return response.sendStatus(201);
+                }
+                default: {
+                    return response.sendStatus(405);
+                }
             }
-            default: {
-                return response.sendStatus(405);
-            }
-        }
-        // });
+        });
     });
 
 // 2. Get Items for Realm

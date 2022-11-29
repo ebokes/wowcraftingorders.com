74f5ddf86cabbadd314488f14f4529ca349ba7ec
Anden Acitelli <a.acitelli@gmail.com>
2022-11-29 11:54:55 -0500

    Axios 404 when checking item should no longer be fatal

--- functions/src/util.ts
+++ functions/src/util.ts
@@ -2,15 +2,19 @@ import axios from "axios";
 import { BattleNetProfileDataResponse } from "./types";
 
 export const itemExists = async (itemId: number, authHeader: string): Promise<boolean> => {
-    const response = await axios.get<BattleNetProfileDataResponse>(`https://us.api.blizzard.com/data/wow/item/${itemId}`, {
-        headers: {
-            "Authorization": authHeader,
-            "Accept-Encoding": "utf-8",
-        },
-        params: {
-            "namespace": "profile-us",
-            "locale": "en_US"
-        }
-    })
-    return response.status === 200;
+    try {
+        await axios.get<BattleNetProfileDataResponse>(`https://us.api.blizzard.com/data/wow/item/${itemId}`, {
+            headers: {
+                "Authorization": authHeader,
+                "Accept-Encoding": "utf-8",
+            },
+            params: {
+                "namespace": "profile-us",
+                "locale": "en_US"
+            }
+        })
+        return true;
+    } catch (err) {
+        return false;
+    }
 };

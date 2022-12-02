189a29ff1d67ee5950f833159152ca58f15cb52c
Anden Acitelli <a.acitelli@gmail.com>
2022-12-02 12:55:50 -0500

    Supposedly better implementation

--- functions/src/middleware.ts
+++ functions/src/middleware.ts
@@ -18,28 +18,42 @@ export const logRequest: RequestHandler = (request, response, next) => {
 }
 
 export const logResponseBody: RequestHandler = (req, res, next) => {
-    const oldWrite = res.write,
-        oldEnd = res.end;
+    const oldWrite = res.write;
+    const oldEnd = res.end;
 
-    const chunks: any[] = [];
+    const chunks: readonly Uint8Array[] | Buffer[] = [];
 
-    res.write = function (chunk) {
-        chunks.push(chunk);
-
+    // @ts-ignore
+    res.write = (...restArgs) => {
         // @ts-ignore
-        return oldWrite.apply(res, arguments);
+        chunks.push(Buffer.from(restArgs[0]));
+        // @ts-ignore
+        oldWrite.apply(res, restArgs);
     };
 
     // @ts-ignore
-    res.end = function (chunk) {
-        if (chunk)
-            chunks.push(chunk);
-
+    res.end = (...restArgs) => {
+        if (restArgs[0]) {
+            // @ts-ignore
+            chunks.push(Buffer.from(restArgs[0]));
+        }
         const body = Buffer.concat(chunks).toString('utf8');
-        console.log(req.path, body);
+
+        console.log({
+            time: new Date().toUTCString(),
+            fromIP: req.headers['x-forwarded-for'] ||
+                req.connection.remoteAddress,
+            method: req.method,
+            originalUri: req.originalUrl,
+            uri: req.url,
+            requestData: req.body,
+            responseData: body,
+            referer: req.headers.referer || '',
+            ua: req.headers['user-agent']
+        });
 
         // @ts-ignore
-        oldEnd.apply(res, arguments);
+        oldEnd.apply(res, restArgs);
     };
 
     next();

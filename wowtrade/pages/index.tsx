46e79daf173104f9970a155c5d68260d14dbc1f1
Anden Acitelli <a.acitelli@gmail.com>
2022-12-02 13:40:20 -0500

    Design tweaks

--- wowtrade/pages/index.tsx
+++ wowtrade/pages/index.tsx
@@ -5,6 +5,7 @@ import ListingsList from "../components/ListingsList";
 import { Form } from "react-bootstrap";
 import { RegionRealmContext } from "./_app";
 import { SetRegionRealmView } from "../components/SetRealms";
+import Link from "next/link";
 
 export const refreshWowheadLinks = () => {
     const inlineScript = document.createElement('script');
@@ -30,15 +31,17 @@ export default function Home() {
             </Head>
 
             <main className={"p-3"}>
-                <h3>Overview</h3>
+                <h3 className={"mt-4"}>Overview</h3>
                 <p>WoWCraftingOrders.com connects buyers and sellers of private work orders, especially high-quality
                     ones. Finding skilled crafters can often be complicated, and trade chat doesn't really solve the
-                    problem, which is where this site comes in..</p>n
+                    problem, which is where this site comes in.</p>
+                <p>To buy an item, stay on this page! To list an item, please check out the <Link
+                    href={"/sell"}>Sell</Link> page.</p>
                 <Form style={{ width: "100%" }}>
                     <SetRegionRealmView/>
                 </Form>
 
-                <h3 className={"mt-4"}>Search Results</h3>
+                <h3 className={"mt-4 mb-0"}>Search Results</h3>
                 <ListingsList/>
             </main>
         </div>
